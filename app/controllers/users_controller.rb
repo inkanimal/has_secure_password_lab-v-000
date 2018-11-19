4415993acb8ecab641c6b96cee24093326fa631e
geoffrey woodcock <theatomproject@gmail.com>
2018-11-19 01:02:57 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,14 @@ class UsersController < ApplicationController
   end 
   
   def create
-    User.create(user_params)
-  end
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to :welcome
+    else
+      redirect_to :new_user
+    end
+
  
   private
  
