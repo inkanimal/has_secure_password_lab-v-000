af53a91c370ce162ac75b259290d6416eacb79be
geoffrey woodcock <theatomproject@gmail.com>
2018-11-19 00:50:27 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,14 @@
 class SessionsController < ApplicationController
   
    def create
-    @user = User.find_by(username: params[:username])
-    return head(:forbidden) unless @user.authenticate(params[:password])
-    session[:user_id] = @user.id
+     @user = User.find_by(username: params[:username])
+     return head(:forbidden) unless @user.authenticate(params[:password])
+     session[:user_id] = @user.id
    end
-end
+  end
  
 # app/models/user.rb
-class User < ActiveRecord::Base
-  has_secure_password
+  class User < ActiveRecord::Base
+    has_secure_password
 end
 end
