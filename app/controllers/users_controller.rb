f0b1429120cd89467a802e544697a9d39f7ec52a
geoffrey woodcock <theatomproject@gmail.com>
2018-11-19 00:51:16 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,14 @@ class UsersController < ApplicationController
   end 
   
   def create
-    
+    User.create(user_params)
+  end
+ 
+  private
+ 
+   def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+   end
   end
   
   
